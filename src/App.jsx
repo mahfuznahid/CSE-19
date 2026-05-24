398b8754afb97e06ffd0fff12680054168cbee35
Mahfuzur Rahman Nahid <mahfuznahid41@gmail.com>
2026-05-24 18:18:26 +0600

    Fixing mobile grid design

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,7 @@ function App() {
         moto="We Code, We Create!"
       />
 
-      <div
-        style={{
-          backgroundImage: `
-      linear-gradient(rgba(229, 231, 235, 0.3) 1px, transparent 1px),
-      linear-gradient(90deg, rgba(229, 231, 235, 0.3) 1px, transparent 1px)
-    `,
-          backgroundSize: "40px 40px",
-          minHeight: "100vh",
-        }}
-      >
+      <div className="bg-grid min-h-screen">
         {/*Body Start*/}
         <Hero />
         <About />
