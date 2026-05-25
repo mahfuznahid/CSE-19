8bcedcffb9182ec2359b535e61d50c39ad2418ae
Mahfuzur Rahman Nahid <mahfuznahid41@gmail.com>
2026-05-25 22:43:06 +0600

    Adding Student Information

--- src/Data/CRData.jsx
+++ src/Data/CRData.jsx
@@ -1,14 +1,18 @@
-import img1 from "../assets/StudentImage/Nahid.jpg";
+import img1 from "../assets/StudentImage/UnknownFemale.png";
+import img2 from "../assets/StudentImage/Nahid.jpg";
+import img3 from "../assets/StudentImage/Arittro.jpg";
+import img4 from "../assets/StudentImage/Neha.jpg";
+import img5 from "../assets/StudentImage/Nailah.jpg";
 
 let PresentCRs = [
   {
-    Image: img1,
+    Image: img3,
     StudentName: "Arittro Hawlader",
     StudentID: "B230305011",
     Email: "arittrohowlader@gmail.com",
   },
   {
-    Image: img1,
+    Image: img4,
     StudentName: "Neha Khan",
     StudentID: "B230305042",
     Email: "shamima143.t@gmail.com",
@@ -17,13 +21,13 @@ let PresentCRs = [
 
 let PreviousCRs = [
   {
-    Image: img1,
+    Image: img2,
     StudentName: "Mahfuzur Rahman Nahid",
     StudentID: "B230305016",
     Email: "mahfuznahid41@gmail.com",
   },
   {
-    Image: img1,
+    Image: img5,
     StudentName: "Nailah Islam",
     StudentID: "B230305029",
     Email: "nailahislam06@gmail.com",
--- src/Data/TopperData.jsx
+++ src/Data/TopperData.jsx
@@ -1,30 +1,34 @@
-import img0 from "../assets/StudentImage/Masum.jpg";
-import img1 from "../assets/StudentImage/Nahid.jpg";
+import img0 from "../assets/StudentImage/UnknownMale.jpg";
+import img1 from "../assets/StudentImage/UnknownFemale.png";
+import img2 from "../assets/StudentImage/Masum.png";
+import img3 from "../assets/StudentImage/Jim.jpg";
+import img4 from "../assets/StudentImage/Mahmud.jpg";
+import img5 from "../assets/StudentImage/Nailah.jpg";
 
 let toppers = [
   {
-    Image: img1,
+    Image: img5,
     StudentName: "Nailah Islam",
     StudentID: "B230305029",
     CGPA: "4.00",
     Position: "1st",
   },
   {
-    Image: img1,
+    Image: img3,
     StudentName: "Mahadi Hasan Jim",
     StudentID: "B230305003",
     CGPA: "3.96",
     Position: "2nd",
   },
   {
-    Image: img1,
+    Image: img4,
     StudentName: "Md Mahmud Ul Hasan",
     StudentID: "B230305010",
     CGPA: "3.89",
     Position: "3rd",
   },
   {
-    Image: img1,
+    Image: img0,
     StudentName: "Md Abrar Foysal Prince",
     StudentID: "B230305013",
     CGPA: "3.84",
@@ -38,7 +42,7 @@ let toppers = [
     Position: "5th",
   },
   {
-    Image: img0,
+    Image: img2,
     StudentName: "Masum Billah",
     StudentID: "B230305001",
     CGPA: "3.82",
