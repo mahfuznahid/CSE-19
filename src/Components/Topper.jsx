f4154ad7ff29d56ad63e8994feb8111fc3fe731c
Mahfuzur Rahman Nahid <mahfuznahid41@gmail.com>
2026-07-31 19:46:46 +0600

    Adding new topper file

--- src/Components/Topper.jsx
+++ src/Components/Topper.jsx
@@ -1,3 +1,4 @@
+import toppers02 from "../Data/TopperData02";
 import toppers from "./../Data/TopperData";
 
 export default function Topper() {
@@ -10,6 +11,43 @@ export default function Topper() {
         </h3>
         <div className="grid gap-4 sm:grid-cols-4">
           {toppers.map((citem) => (
+            <div className="flex bg-white rounded-xl shadow-md hover:shadow-xl transition-all duration-300 p-4">
+              {/* Image */}
+              <img
+                src={citem.Image}
+                alt={citem.StudentName}
+                className="w-24  object-cover rounded-lg mr-4"
+              />
+              <div className="flex flex-col text-left">
+                {/* Name */}
+                <h3 className="text-lg font-bold text-[#1E293B] mb-2">
+                  {citem.StudentName}
+                </h3>
+
+                {/* Details */}
+                <p className="text-sm text-gray-600 mb-1">
+                  <span className="font-semibold">Student ID:</span>{" "}
+                  {citem.StudentID}
+                </p>
+
+                <p className="text-sm text-gray-600 mb-1">
+                  <span className="font-semibold">CGPA:</span> {citem.CGPA}
+                </p>
+
+                <p className="text-sm text-gray-600 mb-3">
+                  <span className="font-semibold">Position:</span>{" "}
+                  {citem.Position}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <h3 className="text-xl text-white font-semibold mb-4">
+          Second Semester
+        </h3>
+        <div className="grid gap-4 sm:grid-cols-4">
+          {toppers02.map((citem) => (
             <div className="flex bg-white rounded-xl shadow-md hover:shadow-xl transition-all duration-300 p-4">
               {/* Image */}
               <img
---
+++ src/Data/TopperData02.jsx
@@ -0,0 +1,53 @@
+import img0 from "../assets/StudentImage/UnknownMale.jpg";
+import img1 from "../assets/StudentImage/UnknownFemale.png";
+import img2 from "../assets/StudentImage/Masum.png";
+import img3 from "../assets/StudentImage/Jim.jpg";
+import img4 from "../assets/StudentImage/Tahmeed.jpg";
+import img5 from "../assets/StudentImage/Nailah.jpg";
+
+let toppers02 = [
+  {
+    Image: img5,
+    StudentName: "Nailah Islam",
+    StudentID: "B230305029",
+    CGPA: "4.00",
+    Position: "1st",
+  },
+  {
+    Image: img2,
+    StudentName: "Masum Billah",
+    StudentID: "B230305001",
+    CGPA: "3.95",
+    Position: "2nd",
+  },
+  {
+    Image: img3,
+    StudentName: "Mahadi Hasan Jim",
+    StudentID: "B230305003",
+    CGPA: "3.94",
+    Position: "3rd",
+  },
+  {
+    Image: img0,
+    StudentName: "Md Abrar Foysal Prince",
+    StudentID: "B230305013",
+    CGPA: "3.87",
+    Position: "4th",
+  },
+  {
+    Image: img1,
+    StudentName: "Tahsina Rahman Momo",
+    StudentID: "B230305048",
+    CGPA: "3.87",
+    Position: "4th",
+  },
+  {
+    Image: img4,
+    StudentName: "Tahmeed Islam",
+    StudentID: "B230305025",
+    CGPA: "3.80",
+    Position: "5th",
+  },
+];
+
+export default toppers02;
